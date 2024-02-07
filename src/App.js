68f6feb94ab0fcf21173d38f835cc2217d2196a2
Joon <JustcallmeHarmony>
2024-02-08 01:41:11 +0900

    데이터 삭제하기

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-function App() {
+const App = () => {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
@@ -17,23 +17,15 @@ function App() {
       created_date,
       id: dataId.current,
     };
-
     dataId.current += 1;
-
     setData([newItem, ...data]);
-  };
-
-  const onDelete = (targetId) => {
-    const newDiaryList = data.filter((it) => it.id !== targetId);
-    setData(newDiaryList);
   };
 
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} onDelete={onDelete} />
+      <DiaryList diaryList={data} />
     </div>
   );
-}
-
+};
 export default App;
